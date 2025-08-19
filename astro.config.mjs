c9d90f118fb3b94fcb3448e93ee891010c0b2e48
HAMZA-KHALID-21 <hamza33857@gmail.com>
2025-08-19 13:17:54 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/HAMZA-KHALID-21/Hamza_Khalid_portfolio',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://HAMZA-KHALID-21.github.io/Hamza_Khalid_portfolio',
+  base: '/Hamza_Khalid_portfolio/',
+  integrations: [mdx(), sitemap(), tailwind()],
 });
